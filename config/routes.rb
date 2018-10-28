141bc8061b781caf802d4dcb1a7312b9c31d015a
Williams Estrada <we4841@gmail.com>
2018-10-27 19:28:45 -0500

    Set multiple Devise models
    Configure Devise admins and users models, set scoped views for each
    model.
    Modify the routes to work with those two models
    Create a concern file to prevent cross model visits

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  # TODO Set authenticated path and set it on app/controllers/concern/accessible.rb
+  # set only routes for sessions
+  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' }
+
+
+  devise_for :admins, path: 'admin', controllers: { sessions: 'admins/sessions' }
+
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
