f4d09c63f04d29a7f17f8c666ed62fc55f1dc058
Williams Estrada <we4841@gmail.com>
2018-10-28 12:59:50 -0600

    Disable sign up routes
    Users and Admins now can just log in to the platform, they can not sign
    up a new user

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
-  # TODO Set authenticated path and set it on app/controllers/concern/accessible.rb
-  # set only routes for sessions
-  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions' }
+
+  # Prevent users new registrations
+  devise_for :users, path: 'users', controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations' 
+  },skip:  [:registrations] 
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
+    put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
 
 
-  devise_for :admins, path: 'admin', controllers: { sessions: 'admins/sessions' }
+  # Prevent admins new registrations
+  devise_for :admins, path: 'admin', controllers: {
+    sessions: 'admins/sessions',
+    registrations: 'admins/registrations' }, skip:  [:registrations] 
+  as :admin do
+    get 'admins/edit' => 'devise/registrations#edit', :as => 'edit_admin_registration'
+    put 'admins' => 'devise/registrations#update', :as => 'admin_registration'
+  end
 
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
