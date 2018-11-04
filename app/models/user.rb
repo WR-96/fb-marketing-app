2b4246cd5903a8dab573bfa80f0f668f2187230e
Williams Estrada <we4841@gmail.com>
2018-11-04 11:08:13 -0600

    Modify User views to match new fields
    Fields user and username were added in the views needed to let create
    and update User model along with these new params.
    Validations were implemented to verify uniqueness of username and its
    format.
    User model were modify to let users sign in with their username instead
    of email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,30 @@
 class User < ApplicationRecord
+  validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+  validates :username, presence: :true, uniqueness: :true
+  attr_writer :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable#, :validatable
+         :recoverable, :rememberable, authentication_keys: [:login]
+  
+
+  def login
+    @login || self.username
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+  conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_hash).where(["lower(username) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username)
+      where(conditions.to_hash).first
+    end
+  end
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 end
