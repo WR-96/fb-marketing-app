be10e448185f94c3ffc0cf06ef797af4f29d562e
Williams Estrada <we4841@gmail.com>
2018-11-10 12:57:37 -0600

    Change campaigns form nested resource to resources
    This change is made since it is needed a view to add campaigns and
    assign them to a company, with the previous implementation a new
    campaign belongs to a certain company and you only can add campaigns to
    that company, now you can select the company for a new campaign.
    
    To make this possible we un-nested the campaign resource in the routes
    files and modified the views to have the correct paths, also modified
    the controller's actions to not depend on the company

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,41 +1,33 @@
 class CampaignsController < ApplicationController
-  before_action :set_company
   before_action :set_campaign, only: [:show, :edit, :update, :destroy]
 
-  # GET /campaigns/1
-  # GET /campaigns/1.json
-  def show
+  def index
+    @campaigns = Campaign.all
   end
 
-  # GET /campaigns/new
   def new
-    @campaign = @company.campaigns.build
+    @campaign = Campaign.new
   end
 
-  # GET /campaigns/1/edit
   def edit
   end
 
-  # POST /campaigns
-  # POST /campaigns.json
   def create
-    @campaign = @company.campaigns.build(campaign_params)
+    @campaign = Campaign.new(campaign_params)
 
     respond_to do |format|
       if @campaign.save
-        format.html { redirect_to company_path(@campaign.company), notice: 'Campaign was successfully created.' }
+        format.html { redirect_to campaigns_path, notice: 'Campaign was successfully created.' }
       else
         format.html { render :new }
       end
     end
   end
 
-  # PATCH/PUT /campaigns/1
-  # PATCH/PUT /campaigns/1.json
   def update
     respond_to do |format|
-      if @campaign.update_params(campaign_params)
-        format.html { redirect_to company_path(@campaign.company), notice: 'Campaign was successfully updated.' }
+      if @campaign.update(campaign_params)
+        format.html { redirect_to campaigns_path, notice: 'Campaign was successfully updated.' }
         format.json { render :show, status: :ok, location: @campaign }
       else
         format.html { render :edit }
@@ -44,27 +36,19 @@ class CampaignsController < ApplicationController
     end
   end
 
-  # DELETE /campaigns/1
-  # DELETE /campaigns/1.json
   def destroy
     @campaign.destroy
     respond_to do |format|
-      format.html { redirect_to company_path(@campaign.company), notice: 'Campaign was successfully destroyed.' }
+      format.html { redirect_to campaigns_path, notice: 'Campaign was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_campaign
-      @campaign = @company.campaigns.find(params[:id])
+      @campaign = Campaign.find(params[:id])
     end
 
-    def set_company
-      @company = Company.find(params[:company_id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def campaign_params
       params.require(:campaign).permit(:name, :goald, :description, :start_date, :end_date, :company_id)
     end
