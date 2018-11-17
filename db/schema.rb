71086a4bcebf65d8ec0c02124e78993783840de1
Williams Estrada <we4841@gmail.com>
2018-11-17 12:38:08 -0600

    Migration to add role filed to users table
    This change is made since a user needs to have specific role in the
    platform to show him different views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_04_160807) do
+ActiveRecord::Schema.define(version: 2018_11_17_183352) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 2018_11_04_160807) do
     t.datetime "updated_at", null: false
     t.string "username"
     t.string "name"
+    t.integer "role"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
   end
